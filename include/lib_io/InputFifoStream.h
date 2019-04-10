1d7121dccd726a496b169963496d72b28626ab4c
Carmelo Pintaudi <c.pintaudi@gmail.com>
2019-04-10 11:15:51 +0200

    [DOC] Document InputFifoStream.h API in doxygen

--- include/lib_io/InputFifoStream.h
+++ include/lib_io/InputFifoStream.h
@@ -27,25 +27,51 @@ struct InputFifoStream
 
 /* Exported constants --------------------------------------------------------*/
 /* Exported functions ------------------------------------------------------- */
-
+/**
+ * @brief constructor. The input fifo stream is a input stream that has a fifo
+ *  buffer to contain the bytes in input.
+ *
+ * @param self pointer to self
+ * @param readBuffer a chunk of memory to be used by the fifo
+ * @param readBufSize size of the memory that the fifo can use
+ *
+ * @return true if success
+ *
+ */
 bool
 InputFifoStream_ctor(InputFifoStream* self, void* readBuf, size_t readBufSize);
-
+/**
+ * @brief static implementation of virtual method Stream_read()
+ *
+ */
 size_t
 InputFifoStream_read(Stream* self, char* buffer, size_t length);
-
+/**
+ * @brief static implementation of virtual method Stream_get()
+ *
+ */
 size_t
 InputFifoStream_get(Stream* self,
                     char* buff,
                     size_t len,
                     const char* delims,
                     unsigned timeoutTicks);
+/**
+ * @brief static implementation of virtual method Stream_available()
+ *
+ */
 size_t
 InputFifoStream_available(Stream* self);
-
+/**
+ * @brief static implementation of virtual method Stream_skip()
+ *
+ */
 void
 InputFifoStream_skip(Stream* self);
-
+/**
+ * @brief static implementation of virtual method Stream_dtor()
+ *
+ */
 void
 InputFifoStream_dtor(Stream* self);
 
